b7b707bec76e51cf07c02bb0a337823d58bedd07
mahmoudmohamed1994 <mahmoud292949@gmail.com>
2023-08-26 18:48:53 +0200

    done with the style

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react"
 import "./style.css"
 
 export default function App() {
+const [newItem, setNewItem] = useState("")
   return (
     <>
     
     <form className="new-items-form">
     <div className="form-row">
       <label htmlFor="item">New Item</label>
-      <input type="text" id="item"/>
+      <input value={newItem} type="text" id="item"/>
     </div>
     <button className="btn">Add</button>
   </form>
